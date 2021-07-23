d9fb23da674c8a04cd38b6f4374fed2ae411af8d
Divyansh Bhati <87819841+AMEXDIV@users.noreply.github.com>
2021-07-23 09:16:00 +0530

    Update python-qr-code-generator.py

--- python-qr-code-generator.py
+++ python-qr-code-generator.py
@@ -1,3 +1,8 @@
 import qrcode
 img = qrcode.make("https://www.youtube.com/channel/UCMUSIgtI6otfdIVm6flvNWA")
-img.save("myyoutubechannel.jpg")
+img.save("image-name.jpg")
+
+
+#before running or saving please install the following pip module 
+# pip install qrcode 
+#👆 run this thing on your command prompt and then run this code.
